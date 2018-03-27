5f99d0b994e2ecb6a22c24e88aa5a15c6f330cba
Nirmal Jacob <Nirmal.Jacob@wolterskluwer.com>
2018-03-27 09:53:14 -0400

    Removed some leftovers

--- src/main/java/com/withoutstreams/MethodsWithoutStreams.java
+++ src/main/java/com/withoutstreams/MethodsWithoutStreams.java
@@ -4,15 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class MethodsWithoutStreams{
-
-  public List<String> flattenSomeCollections(List<List<String>> Animals) {
-        List<String> flatcollection = new ArrayList<>();
-        for(List<String> subCollection : Animals){
-            for (String value : subCollection){
-                flatcollection.add(value);
-
-            }
-        }
-        return flatcollection;
-    }
+    
 }
