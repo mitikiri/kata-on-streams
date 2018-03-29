7fc728f67be7df3e3cca77646aeb16add21d6d3e
NA\Manjunath.Mitikiri <manjunath.mitikiri@wolterskluwer.com>
2018-03-29 14:58:56 -0400

    Streams Java Kata

--- src/main/java/com/withstreams/MostMethodsAreInThisClass.java
+++ src/main/java/com/withstreams/MostMethodsAreInThisClass.java
@@ -1,5 +1,23 @@
 package com.withstreams;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public final class MostMethodsAreInThisClass {
+    public List<String> willConvertToUpperCase(List<String> inputStrings) {
+        return inputStrings.stream().map(String::toUpperCase).collect(toList());
+
+        //        List<String> outputStrings = new ArrayList<>();
+        //        for (String inputString : inputStrings) {
+        //            outputStrings.add(inputString.toUpperCase());
+        //        }
+        //        return outputStrings;
+    }
+
+    public List<String> getPlacesMoreThanTenCharLong(List<String> inputStrings) {
+        return inputStrings.stream().filter(String -> String.length() > 10).collect(toList());
+    }
 
 }
--- src/test/java/com/withoutstreams/MethodsWithoutStreamsTest.java
+++ src/test/java/com/withoutstreams/MethodsWithoutStreamsTest.java
@@ -1,4 +1,46 @@
 package com.withoutstreams;
 
+import com.withstreams.MostMethodsAreInThisClass;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public final class MethodsWithoutStreamsTest {
+    @Test
+    public void verifyUpperCaseConversion() {
+        List<String> inputStrings = new ArrayList<String>();
+        inputStrings.add("StreAmsJava");
+        inputStrings.add("JavaKAta");
+        inputStrings.add("testingSTRINGCONVERSION");
+
+        MostMethodsAreInThisClass mostMethodsAreInThisClass = new MostMethodsAreInThisClass();
+        List<String> outputStrings = mostMethodsAreInThisClass.willConvertToUpperCase(inputStrings);
+        //List<String> expectedString = asList("STREAMSJAVA","JAVAKATA","TESTINGSTRINGCONVERSION");
+        List<String> expectedStrings = new ArrayList<>();
+        for (String inputString : inputStrings) {
+            expectedStrings.add(inputString.toUpperCase());
+        }
+
+        //Assert.assertThat(outputStrings, Is.is(expectedStrings));
+        Assert.assertEquals(outputStrings, expectedStrings);
+    }
+
+    @Test
+    public void verifyGetPlacesMoreThanTenPlacesLong() {
+        List<String> inputStrings = new ArrayList<String>();
+        inputStrings.add("StreAmsJava");
+        inputStrings.add("JavaKAta");
+        inputStrings.add("testingSTRINGCONVERSION");
+
+        MostMethodsAreInThisClass mostMethodsAreInThisClass = new MostMethodsAreInThisClass();
+        List<String> outputStrings = mostMethodsAreInThisClass.getPlacesMoreThanTenCharLong(inputStrings);
+        for(String outputString : outputStrings) {
+            Assert.assertTrue(outputString.length()>10);
+        }
+    }
 }
